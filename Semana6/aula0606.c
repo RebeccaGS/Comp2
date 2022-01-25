caf97316c7d88461defd476dc961f7c7accaaf42
RebeccaGS <Rebecca.gomsim@gmail.com>
2022-01-24 22:06:42 -0300

    BSD 06 defeituoso
    versao sem rodar com 06

--- Semana6/aula0606.c
+++ Semana6/aula0606.c
@@ -12,10 +12,35 @@ $Date$
 $Log$
 */
 
+#define OK										0
+#define NUMERO_ARGUMENTOS_INVALIDO              1
+#define BASE_INVALIDA                           2
+#define VALOR_MAXIMO_EXCEDIDO                   3
+#define CONTEM_CARACTERE_INVALIDO               4
+
+#define EOS				      					'\0'
+
+#include "aula0601.c"
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <float.h> /* DBL_MAX */
+#include <errno.h>
+
+/*
+Crie o arquivo "aula0605.c" contendo o programa de testes para a função CalcularMenorComplementar. O programa deverá receber,  através dos argumentos de linha de comando, a ordem da matriz, os valores de cada elemento desta matriz e os valores correspondentes à linha e à coluna do elemento para o qual se deseja calcular o menor complementar. O programa deverá exibir a matriz (em formato de matriz) recebida via CLI, o elemento em questão e o menor complementar correspondente. Os valores da matriz deverão ser exibidos com no mínimo 5 casas decimais. Este programa NÃO pode utilizar alocação dinâmica de memória. Por enquanto, teste o programa apenas com matrizes de ordem 1, 2 e 3.
+
+*/
+int main (int argc, char **argv){
 
 
 
 
+
+
+
+}
 
 
 
