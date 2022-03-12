38572a9cc5a36bac87ee283f2a7d3708f5a5a709
RebeccaGS <Rebecca.gomsim@gmail.com>
2022-03-11 21:11:08 -0300

    comecei a 9

---
+++ Semana9/aula0901.h
@@ -0,0 +1,12 @@
+#ifndef   _AULA0901_
+#define   _AULA0901_   "@(#)aula0901.h $Revision$"
+
+typedef unsigned char byte;
+
+typedef enum {ok = 0, erroAbrirNomeArquivo = 1 } tipoErros;
+
+tipoErros
+ExibirConteudoArquivo (char * /* (E) */);
+
+#endif
+
